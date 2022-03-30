84036e24f4caeb252b6187b026c46b00170119da
Lucygaio <lucindagaio40@gmail.com>
2022-03-30 11:00:59 +0100

    loja

---
+++ js/index.js
@@ -0,0 +1,17 @@
+
+function sair()
+ {
+	var sair = document.getElementById('menu');
+	if (sair.style.display =='none')
+	{
+		sair.style.display ='block';
+
+	}
+	else
+	{
+		sair.style.display ='none';
+
+	}
+
+}
+
